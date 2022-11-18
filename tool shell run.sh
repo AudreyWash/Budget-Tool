928db6ea2d4df0cda8578a763b39c70597799e01
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-11-18 14:29:08 -0600

    Add files via upload

---
+++ tool shell run.sh
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+from Apples import Apples
+
+low = Apples("Red Apples", 20)
+medium = Apples("Green Apples", 40)
+high = Apples("Purple Apples", 60)
+
+try:
+    appleBudget = float((input("What is your apple budget?")))
+except ValueError:
+    exit("Please enter a number.")
