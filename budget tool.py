c9335d626074b30d699263bd85a2ca8b09cf1c3e
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-11-18 14:10:08 -0600

    Update budget tool.py

--- budget tool.py
+++ budget tool.py
@@ -8,6 +8,16 @@ class Apples:
       print("Invalid entry. Please enter a number.")
       exit()
 
-  
+  def changeAmount(self, budget):
+    return(budget - self.budgetCheck)
+
+  def buy(self, budget):
+    self.budgetCheck(budget)
+    
+    if budget >= self.price:
+      print(f'You can get some apples. {self.name}')
+      
+      if budget == self.price:
+        print("You have exactly enough money for these apples."
     
     
